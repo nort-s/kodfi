11f894d2bc39761f351999e3d3e06bb148ee6c15
Orderic B <barnororderic@gmail.com>
2026-01-11 18:32:16 +0100

    fix: fix password recovering error

---
+++ src/app/(full-width-pages)/(auth)/reset-password/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Mot de Passe Oublié | Kodfi",
+  description: "Changez le mot de passe de votre compte Kodfi.",
+};
+
+export default function ResetPassword() {
+  return <div>mot de passe oublié</div>;
+}
